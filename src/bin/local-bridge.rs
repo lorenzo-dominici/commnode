f2c06547ce7bcf2d95477122723383b728849ea4
lorenzo-dominici <lorenzo.dominici@edu.unifi.it>
2023-11-04 19:25:16 +0100

    start of local bridge development

---
+++ src/bin/local-bridge.rs
@@ -0,0 +1,10 @@
+use commnode;
+use tokio;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    //TODO: network configuration
+    //TODO: local socket channel configuration
+    //TODO: commands protocol
+}
